078dee26bf556696a532635e92fc8647653958c1
knowledgemoor <gabrieldu1180@gmail.com>
2025-11-27 14:03:22 -0300

    chore: removing some unused imports

--- app/App.js
+++ app/App.js
@@ -1,7 +1,6 @@
 import 'react-native-gesture-handler';
 import { AppProvider } from '../context/AppContext';
 import AppNavigator from '../navigation/AppNavigator';
-import '../services/backgroundTaskService';
 
 export default function App() {
   return (
